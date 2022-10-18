e4dc16ce901199394ba61f1cbda6541c20a2061c
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-10-18 12:48:26 -0400

    exercise finished

--- calculator.js
+++ calculator.js
@@ -2,7 +2,10 @@
 // Find DOM element and hold onto it, so we don't have to search for it
 // every time we use it.
 const calcForm = document.getElementById("calc-form");
-
+const loanAmountInput = document.getElementById("loan-amount");
+const termInYearsInput = document.getElementById("loan-years");
+const yearlyRateInput = document.getElementById("loan-rate");
+const resultArea = document.getElementById("calc-monthly-payment");
 
 /** Get form values and return as `{amount, years, rate}`.
  *
@@ -11,25 +14,51 @@ const calcForm = document.getElementById("calc-form");
  * */
 
 function getFormValues() {
+  return {
+    amount: Number(loanAmountInput.value),
+    years: Number(termInYearsInput.value),
+    rate: Number(yearlyRateInput.value)
+  }
+
+
+
 }
 
 
 /** Calculate monthly payment and return exact amount. */
 
 function calcMonthlyPayment(amount, years, rate) {
+  const monthsInYear = 12;
+  const monthlyRate = (rate / 100) / monthsInYear;
+  const n = Math.floor(years * monthsInYear);
+  return (
+      (monthlyRate * amount) /
+      (1 - Math.pow((1 + monthlyRate), -n))
+  );
 }
 
 
 /** Get form values, calculate, convert to 2-decimal places, and update UI. */
 
 function getFormValuesAndDisplayResults() {
+  const {amount, years, rate} = getFormValues();
+  const payment = calcMonthlyPayment(amount,years,rate);
+  let payment1 = payment.toFixed(2)
+  console.log(payment1)
+  resultArea.innerText = "$" + payment1;
+
+
+
 }
 
 
 /** Set initial form values and show initial results. Called at app start. */
 
 function setInitialValues() {
-  // use the default values in the provided screenshot
+  loanAmountInput.value = 10000;
+  termInYearsInput.value = 10;
+  yearlyRateInput.value = 4.5;
+  getFormValuesAndDisplayResults()
 }
 
 
